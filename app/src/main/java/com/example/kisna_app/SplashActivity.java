5df958ee7920ed2f3a8649a387e6656ad9679803
Jay <jaysuthar162001@gmail.com>
2022-10-08 14:56:42 +0530

    THE first commit

---
+++ app/src/main/java/com/example/kisna_app/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.kisna_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent login = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(login);
+                finish();
+            }
+        },3000);
+    }
+}
