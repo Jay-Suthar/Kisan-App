3313a6e6babf5642c437ebe4f27bbbd46127d8dc
Jay <jaysuthar162001@gmail.com>
2022-10-11 21:35:44 +0530

    farmer_product_details added

---
+++ app/src/main/java/com/example/kisna_app/farmer_product_details.java
@@ -0,0 +1,14 @@
+package com.example.kisna_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class farmer_product_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_product_details);
+    }
+}
